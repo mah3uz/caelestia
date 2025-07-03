ce78a6df126dac89eee3c8e341ea82615a6409ab
Mahfuz Shaikh <mah3uz@gmail.com>
2025-07-03 22:42:42 +0600

    Fixed merge conflict

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,17 @@
+set -gx EDITOR nvim
+set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+
 if status is-interactive
     # Starship custom prompt
     starship init fish | source
 
     # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
         echo -en "\e]133;A\e\\"
     end
 end
+
+alias vim="nvim"
